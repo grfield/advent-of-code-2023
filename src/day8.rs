a6bd159fc4db0feaeb2c1497f735bf42de893eb4
Gary Field <gary.field@pobox.com>
2023-12-11 22:11:08 +0000

    Completed Part 2 of Day 8

--- src/day8.rs
+++ src/day8.rs
@@ -6,16 +6,18 @@ struct Map<'a> {
     right: &'a str
 }
 
+type MapType<'a> = HashMap<&'a str, Map<'a>>;
+
 fn main() {
     let input = include_str!("../input/day8.txt");
     advent_of_code_2023::solve_puzzles(input, part1, part2);
 }
 
-fn part1(input: &str) -> u32 {
+fn parse_route_and_map(input: &str) -> (Vec<char>, MapType) {
     let (route_str, maps) = input.split_once("\n\n").unwrap();
     let route = route_str.chars().collect::<Vec<char>>();
 
-    let mut map: HashMap<&str, Map> = HashMap::new();
+    let mut map: MapType = HashMap::new();
     for path in maps.lines() {
         let (key, value) = path.split_once('=').unwrap();
         let (left_str, right_str) = value.split_once(',').unwrap();
@@ -24,26 +26,74 @@ fn part1(input: &str) -> u32 {
         map.insert(key.trim(), Map { left, right: right.trim_start() });
     };
 
-    let mut position = "AAA";
-    let mut steps = 0;
-    while position != "ZZZ" {
-        for step in &route {
-            let instruction: &Map = map.get(position).unwrap();
-            position = match step {
-                'L' => instruction.left,
-                'R' => instruction.right,
-                _ => panic!()
-            };
+    (route, map)
+}
 
-            steps += 1;
-        }
+fn next_node_location<'a>(instruction: &char, map: &'a MapType, node: &str) -> &'a str {
+    let directions: &Map = map.get(node).unwrap();
+
+    match instruction {
+        'L' => directions.left,
+        'R' => directions.right,
+        _ => panic!()
+    }
+}
+
+fn lcm (nums: &[u64]) -> u64 {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd(a, b)
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        return a;
+    }
+
+    gcd(b, a % b)
+}
+
+fn part1(input: &str) -> u32 {
+    let (route, map) = parse_route_and_map(input);
+
+    let mut node = "AAA";
+    let mut counter = 0;
+    let mut steps = 0;
+    while !node.ends_with('Z') {
+        node = next_node_location(&route[counter], &map, node);
+        counter = (counter + 1) % route.len();
+        steps += 1;
     }
 
     steps
 }
 
-fn part2(_input: &str) -> u32 {
-    0
+fn part2(input: &str) -> u64 {
+    let (route, map) = parse_route_and_map(input);
+
+    let nodes = map.keys()
+        .filter(|k| k.ends_with('A'))
+        .cloned()
+        .collect::<Vec<&str>>();
+    let mut ghost_steps: Vec<u64> = Vec::new();
+    for n in nodes {
+        let mut node = n;
+        let mut counter = 0;
+        let mut steps = 0;
+        while !node.ends_with('Z') {
+            node = next_node_location(&route[counter], &map, node);
+            counter = (counter + 1) % route.len();
+            steps += 1;
+        }
+
+        ghost_steps.push(steps);
+    }
+
+    let boxed: Box<[u64]> = ghost_steps.into_boxed_slice();
+    lcm(&boxed)
 }
 
 
@@ -53,19 +103,19 @@ mod tests {
 
     #[test]
     fn test_part_1a_input() {
-        let input_data = include_str!("../example/day8a.txt");
+        let input_data = include_str!("../example/day8a_part1.txt");
         assert_eq!(part1(input_data), 2);
     }
 
     #[test]
     fn test_part_1b_input() {
-        let input_data = include_str!("../example/day8b.txt");
+        let input_data = include_str!("../example/day8b_part1.txt");
         assert_eq!(part1(input_data), 6);
     }
 
     #[test]
     fn test_part_2_input() {
-        let input_data = include_str!("../example/day8a.txt");
-        assert_eq!(part2(input_data), 0);
+        let input_data = include_str!("../example/day8_part2.txt");
+        assert_eq!(part2(input_data), 6);
     }
 }
